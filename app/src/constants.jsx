ae25c71ae27b8437e6a9b24d01965445656b49e8
NekoCL <kiririnkirinoshi@gmail.com>
2022-02-21 17:18:38 +1030

    app

---
+++ app/src/constants.jsx
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x8b7c870aAc1A8E6765AbA6de6D3F9225F0DECbD6';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
